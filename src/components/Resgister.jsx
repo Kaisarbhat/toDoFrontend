53f14bd8bf18c9ad2b6eae7d231bd8ffc9f881dc
K4151R <kaisarahmadbhat1108@gmail.com>
2024-07-08 13:05:57 +0530

    ToDoList  added

---
+++ src/components/Resgister.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/users/register', { username, password });
+      history.push('/login');
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
